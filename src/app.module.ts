2fb64d8d01c3c8a8bad870da2cec5a60b677eded
Rajat <rajatky3107@gmail.com>
2023-04-17 15:59:35 +0530

    Added Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { UserModule } from './user-module/user.module';
+
+// @Module({
+//   imports: [UserModule],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule { }
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user-module/user.module';
+import { TaskModule } from './task-module/task.module';
+// import { DatabaseModule } from './database/database.module';
+// import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { CreateDocument } from './swagger/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +11,10 @@ async function bootstrap() {
       transform: true
     })
   )
+
+  app.setGlobalPrefix('/api/v1')
+  // const configService = app.get(ConfigService)
+  SwaggerModule.setup('api', app, CreateDocument(app))
   await app.listen(3000);
 }
 bootstrap();
