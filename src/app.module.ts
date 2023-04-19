5cd983035f15a9f0d76b68caacf638c2b7e3c22c
Rajat <rajatky3107@gmail.com>
2023-04-19 14:51:21 +0530

    introducing typeorm with mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,13 +13,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomerModule } from './customer/customer.module';
 import { DatabaseModule } from './database/database.module';
 import { TaskModule } from './task-module/task.module';
 // import { DatabaseModule } from './database/database.module';
 // import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [TaskModule, DatabaseModule],
+  imports: [TaskModule, CustomerModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,11 +1,35 @@
+// import { Module } from '@nestjs/common';
+// import { MongooseModule } from '@nestjs/mongoose';
+
+// @Module({
+//     imports: [
+//         MongooseModule.forRoot('mongodb://localhost:27017/task', { useNewUrlParser: true })
+//     ],
+//     controllers: [],
+//     providers: [],
+// })
+// export class DatabaseModule { }
+
+
+// For SQL
+
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Customer } from './entity/customer.entity';
+import { User } from './entity/user.entity';
 
 @Module({
     imports: [
-        MongooseModule.forRoot('mongodb://localhost:27017/task', { useNewUrlParser: true })
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'mysql',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            database: 'test',
+            entities: [User, Customer],
+            synchronize: true,
+        }),
     ],
-    controllers: [],
-    providers: [],
 })
 export class DatabaseModule { }
